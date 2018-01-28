a122b971fcdf5c0f3785e5f07a0d4db68f16151d
manatlan <manatlan@gmail.com>
2018-01-28 20:10:07 +0100

    initial import

---
+++ reqman.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import yaml,os,json,sys,httplib,urllib,ssl,sys,fnmatch,urlparse,glob
+
+os.chdir(os.path.join(os.path.dirname(__file__),"."))
+class SyntaxException(Exception):pass
+###########################################################################
+## http access
+###########################################################################
+class Request:
+    def __init__(self,protocol,host,port,method,path,data=None,headers={}):
+        self.protocol=protocol
+        self.host=host
+        self.port=port
+        self.method=method
+        self.path=path
+        self.data=data
+        self.headers=headers
+    def __repr__(self):
+        return "[%s %s %s]" % (self.protocol.upper(),self.method,self.path)
+
+class Response:
+    def __init__(self,r):
+        self.status = r.status
+        self.content = r.read()
+        self.headers = dict(r.getheaders())
+    def __repr__(self):
+        return "[%s]" % (self.status)
+        #~ return "[%s %s]" % (self.status,self.headers)
+        #~ return "<%s %s : %s>" % (self.status,self.headers,self.content.encode("string_escape"))
+
+def http(r):
+    if r.protocol=="https":
+        cnx=httplib.HTTPSConnection(r.host,r.port,context=ssl._create_unverified_context())
+    else:
+        cnx=httplib.HTTPConnection(r.host,r.port)
+    cnx.request(r.method,r.path,r.data,r.headers)
+    return Response(cnx.getresponse())
+
+###########################################################################
+## Reqs manage
+###########################################################################
+class TestResult(list):
+    def __init__(self,req,res,tests):
+        self.req=req
+        self.res=res
+        results=[]
+        for test in tests:
+            what,value = test.keys()[0],test.values()[0]
+
+            testname = "%s = %s" % (what,value)
+            if what=="status":  result = int(value)==int(self.res.status)
+            elif what=="content": result = value in self.res.content
+            else: result = value in self.res.headers.get(what,"")
+
+            results.append( (testname,result) )
+
+        list.__init__(self,results)
+
+    def __repr__(self):
+        print " -",self.req,"--->",self.res
+        for testname,result in self:
+            print "   - TEST:",testname,"?",result
+        return ""
+
+class Req(object):
+    def __init__(self,method,path,body=None,headers={},tests=[]):  # body = str ou dict ou None
+        if body and not isinstance(body,basestring): body=json.dumps(body)
+
+        self.method=method.upper()
+        self.path=path
+        self.body=body
+        self.headers=headers
+        self.tests=tests
+
+    def test(self,env=None):
+
+        def rep(txt):
+            if env and txt:
+                for key,value in env.items():
+                    if isinstance(value,basestring):
+                        txt=txt.replace("{{%s}}"%key, value.encode('string_escape') )
+            return txt
+
+        if env and "root" in env:
+            h=urlparse.urlparse( env["root"] )
+        else:
+            h=urlparse.urlparse( self.path )
+
+        headers=env.get("headers",{}).copy() if env else {}
+        headers.update(self.headers)
+        for k in headers:
+            headers[k]=rep(headers[k])
+
+        req=Request(h.scheme,h.hostname,h.port,self.method,rep(self.path),rep(self.body),headers)
+        res=http( req )
+
+        return TestResult(req,res,self.tests)
+
+
+    def __repr__(self):
+        return "<%s %s>" % (self.method,self.path)
+
+class Reqs(list):
+    def __init__(self,fd):
+        self.name = fd.name.replace("\\","/")
+        l=yaml.load( fd.read() )
+        ll=[]
+        if l:
+            l=[l] if type(l)==dict else l
+
+            for d in l:
+                if "GET" in d: method,path="GET",d.get("GET","")
+                elif "PUT" in d: method,path="PUT",d.get("PUT","")
+                elif "POST" in d: method,path="POST",d.get("POST","")
+                elif "DELETE" in d: method,path="DELETE",d.get("DELETE","")
+                else: raise SyntaxException("no known verbs")
+                ll.append( Req(method,path,d.get("body",None),d.get("headers",[]),d.get("tests",[])) )
+        list.__init__(self,ll)
+
+def makeTests(env,match="*"):
+    ll=[Reqs(file(i)) for i in fnmatch.filter( sorted(glob.glob("*.yml")+glob.glob("*/*.yml")+glob.glob("*/*/*.yml")), match) ]
+    for f in ll:
+        print f.name,len(f)
+
+        for t in f:
+            print t.test( env )
+
+###########################################################################
+## Environnements
+###########################################################################
+def loadEnv(name=None):
+    env=yaml.load( file("reqman.conf").read() ) if os.path.isfile("reqman.conf") else {}
+    if name in env: env.update( env[name] )
+    if "oauth2" in env:
+        u = urlparse.urlparse(env["oauth2"]["url"])
+        req=Request(u.scheme,u.hostname,u.port,"POST",u.path,urllib.urlencode(env["oauth2"]["params"]),{'Content-Type': 'application/x-www-form-urlencoded'})
+        res=http(req)
+        token=json.loads(res.content)
+        env["headers"]["Authorization"] = token["token_type"]+" "+token["access_token"]
+        print "OAuth2 TOKEN:",env["headers"]["Authorization"]
+    return env
+
+if __name__=="__main__":
+    #~ makeTests( loadEnv("ROA") )
+    #~ makeTests( loadEnv("GATEWAY") )
+    makeTests( loadEnv(),"*nas*" )
---
+++ tests.py
@@ -0,0 +1,174 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import unittest
+import reqman
+import json
+from StringIO import StringIO
+
+################################################################## mock
+def myhttp(q):
+    r=StringIO("the content")
+    r.getheaders=lambda: {"content-type":"plain/text","server":"mock"}
+    r.status=200
+    return reqman.Response(r)
+
+reqman.http = myhttp
+##################################################################
+
+class Tests_Req(unittest.TestCase):
+
+    def test_noenv(self):
+        r=reqman.Req("get","https://github.com/")
+        s=r.test()
+        self.assertEqual(s.res.status, 200)
+
+    def test_simplest_env(self):
+        env=dict(root="https://github.com/")
+
+        r=reqman.Req("Get","/")
+        s=r.test(env)
+        self.assertEqual(s.res.status, 200)
+
+    def test_http(self):
+        env=dict(root="http://github.com/")
+
+        r=reqman.Req("GeT","/")
+        s=r.test(env)
+        self.assertEqual(s.res.status, 200)
+
+    def test_env_headers_inheritance(self):
+        env=dict(root="https://github.com/",headers=dict(h1="my h1"))
+
+        r=reqman.Req("GET","/",headers=dict(h2="my h2"))
+        s=r.test(env)
+        self.assertEqual("h1" in s.req.headers, True)
+        self.assertEqual("h2" in s.req.headers, True)
+
+    def test_Tests(self):
+        env=dict(root="https://github.com/")
+
+        r=reqman.Req("GET","/",tests=[dict(status=200),dict(content="content"),dict(server="mock")])
+        s=r.test(env)
+        self.assertEqual(s.res.status, 200)
+        self.assertEqual( len(s), 3)
+        self.assertEqual(s[0][1], True)
+        self.assertEqual(s[1][1], True)
+        self.assertEqual(s[2][1], True)
+
+    def test_env_var_in_path(self):
+        env=dict(root="https://github.com/",a_var="explore")
+
+        r=reqman.Req("GET","/{{a_var}}")
+        s=r.test(env)
+        self.assertEqual(s.res.status, 200)
+        self.assertEqual("content" in s.res.content, True)
+
+
+class Tests_Reqs(unittest.TestCase):
+
+    def test_simplest_yml(self):
+        f=StringIO("GET: https://github.com/")
+        f.name="yo"
+        l=reqman.Reqs(f)
+        self.assertEqual( len(l), 1)
+
+
+    def test_yml(self):
+        y="""
+- GET: https://github.com/
+- PUT: https://github.com/explore
+- DELETE: https://github.com/explore
+- POST: https://github.com/explore
+"""
+        f=StringIO(y)
+        f.name="filename"
+        l=reqman.Reqs(f)
+        self.assertEqual( len(l), 4)
+        self.assertEqual( [i.method for i in l], ['GET', 'PUT', 'DELETE', 'POST'])
+
+    def test_bad_yml(self):
+        f=StringIO("")
+        f.name="filename"
+
+        self.assertEqual(reqman.Reqs(f),[])
+
+    def test_bad_yml2(self):
+        y="""
+- NOOP: https://github.com/
+"""
+        f=StringIO(y)
+        f.name="filename"
+
+        self.assertRaises(reqman.SyntaxException, lambda: reqman.Reqs(f))
+
+    def test_yml_test_env(self):
+
+        y="""
+- GET: /
+  headers:
+    h2: my h2
+- POST: /{{a_var}}
+  body: "{{a_var}}"
+- PUT: /
+  headers:
+    h3: "{{a_var}}"
+  body:                     # yaml -> json body !
+    var: 1
+    txt: "{{a_var}}"
+"""
+        f=StringIO(y)
+        f.name="filename"
+        l=reqman.Reqs(f)
+        self.assertEqual( len(l), 3)
+        get=l[0]
+        post=l[1]
+        put=l[2]
+        self.assertEqual( get.path, "/")
+        self.assertEqual( get.body, None)
+        self.assertEqual( get.headers, {'h2': 'my h2'})
+
+        self.assertEqual( post.path, "/{{a_var}}")
+        self.assertEqual( post.body, "{{a_var}}")
+        self.assertEqual( post.headers, [])
+
+        self.assertEqual( put.path, "/")
+        self.assertEqual( json.loads(put.body), {"var": 1, "txt": "{{a_var}}"})
+        self.assertEqual( put.headers, {'h3': '{{a_var}}'})
+
+        #=-
+
+        env=dict(
+            root="https://github.com/",
+            headers=dict(h1="my h1"),
+            a_var="explore",
+        )
+
+        s=get.test(env)
+        self.assertEqual( s.req.host, "github.com")
+        self.assertEqual( s.req.protocol, "https")
+        self.assertEqual( s.req.port, None)
+        self.assertEqual( s.req.path, "/")
+        self.assertEqual( s.req.data, None)
+        self.assertEqual( s.req.headers,  {'h1': 'my h1', 'h2': 'my h2'})
+
+        s=post.test(env)
+        self.assertEqual( s.req.host, "github.com")
+        self.assertEqual( s.req.protocol, "https")
+        self.assertEqual( s.req.port, None)
+        self.assertEqual( s.req.path, "/explore")
+        self.assertEqual( s.req.data, "explore")
+        self.assertEqual( s.req.headers,  {'h1': 'my h1'})
+
+        s=put.test(env)
+        self.assertEqual( s.req.host, "github.com")
+        self.assertEqual( s.req.protocol, "https")
+        self.assertEqual( s.req.port, None)
+        self.assertEqual( s.req.path, "/")
+        self.assertEqual( json.loads(s.req.data), {"var": 1, "txt": "explore"})
+        self.assertEqual( s.req.headers,  {'h1': 'my h1',"h3":"explore"})
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
