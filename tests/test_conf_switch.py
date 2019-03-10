c3c344c4ea7cea226579df9ead607903e3d325c6
manatlan <manatlan@gmail.com>
2019-03-10 20:40:59 +0100

    maj tests

---
+++ tests/test_conf_switch.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from context import client
+
+
+SERVER={
+    "GET http://jim/yo" : lambda q: dict( status=200, body="dsq"),
+    "GET http://jom/yo" : lambda q: dict( status=200, body="dsq"),
+}
+
+FILES=[
+    dict(name="reqman.conf",content="""
+root: http://jim/
+
+switch1: 
+    root: http://jom/
+"""),
+    dict(name="test.yml",content="""
+- GET: /yo
+  tests:
+    - status: 200
+
+"""),
+]
+
+def test_1(client):
+    x=client( )
+    assert x.code==-1
+    assert "-switch1 :" in x.console
+
+    x=client( "." )
+    assert x.code==0
+    assert x.inproc.ok == x.inproc.total
+
+    x=client( ".","-switch1" )
+    assert x.code==0
+    assert x.inproc.ok == x.inproc.total
+
