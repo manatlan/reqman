d59bcbf2e4c334a0a2d4c60be158e93bcc9d0af7
manatlan <manatlan@gmail.com>
2019-09-10 20:04:10 +0200

    add a begin in rmr tests

--- tests/test_940_rmr.py
+++ tests/test_940_rmr.py
@@ -2,6 +2,8 @@ import reqman,pytest,sys,os
 
 
 mock = {
+  "http://a/start":(200,"ok"),
+  "http://b/start":(200,"ok"),
   "http://a/hello":(200,"ok"),
   "http://b/hello":(200,"ok"),
 }
@@ -19,6 +21,7 @@ def _continue(exe):
     x=exe(".","--s",fakeServer=mock)  # generate a RMR
     assert "http://a/hello" in x.console
     assert x.rc==0
+    assert x.console.count("content contains")==2
     assert os.path.isfile("reqman.html")
     os.unlink("reqman.html")
     lrmr=[i for i in os.listdir(".") if i.endswith(".rmr")]
@@ -30,24 +33,28 @@ def _continue(exe):
     assert "http://a/hello" not in x.console # because nothing is replayed
     assert x.rc==0
     assert os.path.isfile("r1.html")
+    assert not x.console.count("content contains")==2 # nothing is replayed, only output
 
     x=exe(lrmr[0],"-other","--o:r2.html",fakeServer=mock) # replay RMR with another switch  -> single result
     assert "http://b/hello" in x.console
     assert "http://a/hello" not in x.console
     assert x.rc==0
     assert os.path.isfile("r2.html")
+    assert x.console.count("content contains")==2
 
     x=exe(lrmr[0],"+other","--o:r3.html",fakeServer=mock) # compare RMR with another switch  -> dual result
     assert "http://b/hello" in x.console
     assert "http://a/hello" not in x.console #because A is not replayed
     assert x.rc==0
     assert os.path.isfile("r3.html")
+    assert x.console.count("content contains")==2
 
     x=exe(lrmr[0],"--r","--o:r4.html",fakeServer=mock) # REPLAY the RMR, and compare old and new -> dual
     assert "http://b/hello" not in x.console
     assert "http://a/hello" in x.console
     assert x.rc==0
     assert os.path.isfile("r4.html")
+    assert x.console.count("content contains")==2
 
 
 def test_COMMAND_rmr_tests_old_switchs(exe):   #<- it's not a real test ... just COPY/PASTE this one for next tests
@@ -60,6 +67,13 @@ def test_COMMAND_rmr_tests_old_switchs(exe):   #<- it's not a real test ... just
         other:
           root: http://b
           def: B
+
+        BEGIN:
+        - GET: /start
+          doc: ici <<def>>
+          tests:
+            - status: 200
+            - content: ok
 
         """)
     _continue(exe)
@@ -76,5 +90,11 @@ def test_COMMAND_rmr_tests_new_switchs(exe):   #<- it's not a real test ... just
             root: http://b
             def: B
 
+        BEGIN:
+        - GET: /start
+          doc: ici <<def>>
+          tests:
+            - status: 200
+            - content: ok
         """)
     _continue(exe)
