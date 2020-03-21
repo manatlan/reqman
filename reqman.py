d6d7fd68f9dca7b84652ae4b489b267fba5f7246
manatlan <manatlan@gmail.com>
2020-03-21 15:42:26 +0100

    r2.3.5 : make faulty tests if invalid

--- reqman.py
+++ reqman.py
@@ -33,7 +33,7 @@ import stpl  # see "pip install stpl"
 import xpath # see "pip install py-dom-xpath-six"
 
 #95%: python3 -m pytest --cov-report html --cov=reqman .
-__version__="2.3.4.0" #only SemVer (the last ".0" is win only)
+__version__="2.3.5.0" #only SemVer (the last ".0" is win only)
 
 
 try:  # colorama is optionnal
@@ -1262,6 +1262,8 @@ class TestResult(list):
                         bool=False
                     if bool:
                         break
+
+                bool=bool and status!=None # make test KO if status is invalid 
 
                 if len(values) == 1:
                     test, opOK, opKO, val = bool, opOK, opKO, value
