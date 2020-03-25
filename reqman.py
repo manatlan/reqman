4e74c47b3086a4069493974f7acbf2acf37287d4
manatlan <manatlan@gmail.com>
2020-03-25 10:11:31 +0100

    Update reqman.py
    increase limited size of html output body to 8192

--- reqman.py
+++ reqman.py
@@ -33,7 +33,7 @@ import stpl  # see "pip install stpl"
 import xpath # see "pip install py-dom-xpath-six"
 
 #95%: python3 -m pytest --cov-report html --cov=reqman .
-__version__="2.3.6.0" #only SemVer (the last ".0" is win only)
+__version__="2.3.7.0" #only SemVer (the last ".0" is win only)
 
 
 try:  # colorama is optionnal
@@ -1584,7 +1584,7 @@ def render(rr:Result) -> str:
         HEADERVALUE = 512
         DOC         = 1024
         TITLE       = 1024
-        BODY        = 4096
+        BODY        = 8192
 
     def limit(txt: str, size=None) -> str:
         if size and txt and len(txt)>int(size):
