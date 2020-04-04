20046f566df5f96ac7b6002034324e5dd20c245b
manatlan <manatlan@gmail.com>
2020-04-04 10:50:50 +0200

    test_autos.py runnable as py3 too

--- REALTESTS/test_autos.py
+++ REALTESTS/test_autos.py
@@ -1,7 +1,28 @@
-import reqman
-import fakereqman
-import sys,time
+import sys,os,io,contextlib
+if __name__=="__main__":
+    sys.path.insert(0,os.path.dirname(os.path.dirname(__file__)))
+    import fakereqman,reqman
+else:
+    import fakereqman,reqman
+
 from glob import glob
+
+
+
+def run(params):
+    sys.argv=params
+    print("\n"+">"*80)
+    print(">"," ".join(sys.argv))
+    print(">"*80)
+
+    fo,fe = io.StringIO(),io.StringIO()
+    with contextlib.redirect_stderr(fe):
+        with contextlib.redirect_stdout(fo):
+            err=fakereqman.main(runServer=False)
+    print(err or "ok")
+
+    output=fo.getvalue()+fe.getvalue()
+    return err
 
 def test_autos():
     """
@@ -28,20 +49,15 @@ def test_autos():
             # remove "--b" to avoid opening tabs
             if "--b" in args:
                 args.remove("--b")
-                args.append("--k")
 
             # and do the tests with optionnal "valid:x:x:x"
-            sys.argv=["FAKEREQMAN",f] + args
-            print("\n"+">"*80)
-            print(">"," ".join(sys.argv))
-            print(">"*80)
-            err=fakereqman.main(runServer=False)
+            err=run(["FAKEREQMAN",f] + args)
             assert err=="", "File '%s' : %s" % (f,err)
-            sys.argv=["FAKEREQMAN",f] + args + ["--o"]
-            print("\n"+">"*80)
-            print(">"," ".join(sys.argv))
-            print(">"*80)
-            err=fakereqman.main(runServer=False)
+
+            err=run(["FAKEREQMAN",f] + args + ["--o"])
             assert err=="", "File '%s' : %s" % (f,err)
     finally:
         ws.stop()
+
+if __name__=="__main__":
+    test_autos()
