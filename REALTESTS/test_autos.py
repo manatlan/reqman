9b1b4959b2b168c11864399b45ddee00d7ea7aed
manatlan <manatlan@gmail.com>
2020-04-04 11:17:17 +0200

    real pytest tests for REALTESTS ;-)

--- REALTESTS/test_autos.py
+++ REALTESTS/test_autos.py
@@ -1,63 +1,50 @@
 import sys,os,io,contextlib
-if __name__=="__main__":
-    sys.path.insert(0,os.path.dirname(os.path.dirname(__file__)))
-    import fakereqman,reqman
-else:
-    import fakereqman,reqman
-
 from glob import glob
+import pytest
+import fakereqman, reqman
 
-
-
-def run(params):
-    sys.argv=params
-    print("\n"+">"*80)
-    print(">"," ".join(sys.argv))
-    print(">"*80)
-
-    fo,fe = io.StringIO(),io.StringIO()
-    with contextlib.redirect_stderr(fe):
-        with contextlib.redirect_stdout(fo):
-            err=fakereqman.main(runServer=False)
-    print(err or "ok")
-
-    output=fo.getvalue()+fe.getvalue()
-    return err
-
-def test_autos():
-    """
-    Will automatically run reqman'tests (auto*.yml) in current folder
-    and check if valid according shebang's valid statements
-    """
-    ll=glob("REALTESTS/auto_*.yml")
-    assert ll
-
+ws=None
+def setup_module():
+    global ws
     ws=fakereqman.FakeWebServer(11111)
     ws.start()
     import time
     time.sleep(1)   
 
-    try:  
-        for f in ll:
-            txt=reqman.FString(f)
-            firstLine=txt.splitlines()[0]
-            
-            # get args from the shebang on the yaml
-            cmd,params=(firstLine.split("reqman.py"))
-            args=params.strip().split(" ")
+def teardown_module():
+    global ws
+    ws.stop()
 
-            # remove "--b" to avoid opening tabs
-            if "--b" in args:
-                args.remove("--b")
+@pytest.mark.parametrize('file', glob("REALTESTS/auto_*.yml") )
+def test_file(file):
+    txt=reqman.FString(file)
+    firstLine=txt.splitlines()[0]
+    
+    # get args from the shebang on the yaml
+    cmd,params=(firstLine.split("reqman.py"))
+    args=params.strip().split(" ")
 
-            # and do the tests with optionnal "valid:x:x:x"
-            err=run(["FAKEREQMAN",f] + args)
-            assert err=="", "File '%s' : %s" % (f,err)
+    # remove "--b" to avoid opening tabs
+    if "--b" in args: args.remove("--b")
 
-            err=run(["FAKEREQMAN",f] + args + ["--o"])
-            assert err=="", "File '%s' : %s" % (f,err)
-    finally:
-        ws.stop()
+    # and do the tests with optionnal "valid:x:x:x"
+    err=run(["FAKEREQMAN",file] + args)
+    assert err=="", "File '%s' : %s" % (file,err)
 
-if __name__=="__main__":
-    test_autos()
+    err=run(["FAKEREQMAN",file] + args + ["--o"])
+    assert err=="", "File '%s' : %s" % (file,err)    
+
+
+def run(params):
+    sys.argv=params
+
+    fo,fe = io.StringIO(),io.StringIO()
+    with contextlib.redirect_stderr(fe):
+        with contextlib.redirect_stdout(fo):
+            err=fakereqman.main(runServer=False)
+    print(">"," ".join(sys.argv),"--->",err or "ok")
+
+    output=fo.getvalue()+fe.getvalue()
+    return err
+
+
