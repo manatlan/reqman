a681ff0c4b034144614a13f8b9c1ff6a9144bb49
manatlan <manatlan@gmail.com>
2021-03-05 12:49:54 +0100

    better

--- tests/test_991_exposes.py
+++ tests/test_991_exposes.py
@@ -353,7 +353,7 @@ def test_no_input(exe):
 """)
 
     x=exe(".",fakeServer=MOCK)
-    x.view()
+    # x.view()
     assert x.rc == 0 # all ok
 
 
@@ -382,5 +382,5 @@ obj: [1,2,3]
 """)
 
     x=exe(".",fakeServer=MOCK)
-    x.view()
+    # x.view()
     assert x.rc == 0 # all ok
