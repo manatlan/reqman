4519b2fbcc69a43a484a8787297023993b576093
manatlan <manatlan@gmail.com>
2025-10-03 18:24:11 +0200

    pic

--- buildWinExe.py
+++ buildWinExe.py
@@ -30,7 +30,6 @@ try:
     exe_name=f"reqman-{reqman.__version__}.exe"
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-    # build PyInstaller arguments and detect UPX
     args = [
         'src/reqman/__main__.py',
         "-n", exe_name[:-4],
@@ -38,17 +37,6 @@ try:
         "--exclude-module", "tkinter",
         '--icon=ressources/reqman.ico'
     ]
-
-    upx_path = shutil.which('upx') or shutil.which('upx.exe')
-    if upx_path:
-        upx_dir = os.path.dirname(os.path.abspath(upx_path))
-
-    if upx_dir:
-        print(f"Using UPX from {upx_dir}")
-        args.append(f"--upx-dir={upx_dir}")
-    else:
-        print("UPX not found or disabled; building without UPX")
-        args.append('--noupx')
 
     py.run(args)
 
